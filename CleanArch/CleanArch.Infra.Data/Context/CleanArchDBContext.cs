c9c1b43dc81b13d63a9806d816d4a9b8c085e6ab
Marcus Fernando Correa Barbosa <marcus.barbosa.ext@rendimento.com.br>
2019-09-10 15:27:21 -0300

    DataLayer CleanArchDBContext Domain, Update MVC for new CleanArchDBContext

---
+++ CleanArch/CleanArch.Infra.Data/Context/CleanArchDBContext.cs
@@ -0,0 +1,19 @@
+﻿using CleanArch.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Infra.Data.Context
+{
+    public class CleanArchDBContext : DbContext
+    {
+        public CleanArchDBContext(DbContextOptions options)
+            :base(options)
+        {
+
+        }
+        public DbSet<Course> Courses { get; set; }
+
+    }
+}
