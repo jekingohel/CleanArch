91d588e85fd0478e5be0cda2706cb02522bd0d1f
Marcus Fernando Correa Barbosa <marcus.barbosa.ext@rendimento.com.br>
2019-09-11 09:49:17 -0300

    Creating Interace for Application and Repository

---
+++ CleanArch/CleanArche.Application/Interfaces/ICourseService.cs
@@ -0,0 +1,12 @@
+﻿using CleanArche.Application.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArche.Application.Interfaces
+{
+    public interface ICourseService
+    {
+        IEnumerable<CourseViewModel> GetCourses();
+    }
+}
---
+++ CleanArch/CleanArche.Application/ViewModels/CourseViewModel.cs
@@ -0,0 +1,10 @@
+﻿using CleanArch.Domain.Entities;
+using System.Collections.Generic;
+
+namespace CleanArche.Application.ViewModels
+{
+    public class CourseViewModel
+    {
+        public IEnumerable<Course> Courses { get; set; }
+    }
+}
